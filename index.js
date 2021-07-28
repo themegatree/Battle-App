ef0f1de6b9ad11a8e14a4fc8c4579ae4e4d650d3
Shivam Sheth <shivam@shethmail.co.uk>
2021-07-28 16:00:46 +0100

    0.5.1 - no progress

--- index.js
+++ index.js
@@ -7,28 +7,36 @@ const Player = require('./src/player.js');
 let player1, player2;
 
 app.set('view engine','ejs')
-app.use(express.urlencoded({ extended : true}))
+app.use(express.urlencoded({extended : true}))
 
 app.get('/', (req, res) => {
   res.render("index.ejs")
 })
 app.post('/player1', (req, res) => {
-  player1 = new Player(req.body.inputplayer1);
-  player2 = new Player(req.body.inputplayer2);
+    player1 = new Player(req.body.inputplayer1);
+    player2 = new Player(req.body.inputplayer2);
 
     res.render("player1.ejs", {
         inputplayer1: player1.name,
         inputplayer2: player2.name,
         player2HP: player2.HP,
     })
-})
-app.post('/player1', (req, res) => {
-  console.log("here")
 
+})
+
+// app.get("/player1-2", (req,res) => {
+//   res.render("player1.ejs", {
+//     inputplayer1: player1.name,
+//     inputplayer2: player2.name,
+//     player2HP: player2.HP,
+//   })
+// })
+
+app.post('/player1', (req, res) => {
   res.render("player1.ejs", {
-    inputplayer1: player1.name,
-    inputplayer2: player2.name,
-    player2HP: player2.HP,
+    inputplayer1: "P1",
+    inputplayer2: "P2",
+    player2HP: 15,
   })
 })
 
